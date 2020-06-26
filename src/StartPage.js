029c9c4f63e8bf1de3fd352850d13494cc866e8d
Linnea Rudenius <linnea.rudenius@epidemicsound.com>
2020-06-26 12:50:48 +0200

    Require to get image in start page

--- src/StartPage.js
+++ src/StartPage.js
@@ -2,39 +2,58 @@
 import { jsx } from "@emotion/core";
 
 const StartPage = () => (
-    <div css={{
-        background: 'url("./no_way_through.jpg") no-repeat center center fixed',
-        '-webkit-background-size': 'cover',
-        '-moz-background-size': 'cover',
-        '-o-background-size': 'cover',
-        height: '100vh',
-        with: '100vw'
-
-    }}>
-        <div css={{
-            position: 'absolute',
-            bottom: 5,
-            color: '#eabe53',
-            fontFamily: 'Indie Flower, cursive',
-            padding: '0 10px',
-            backgroundColor: '#22291f',
-        }}>
-            Linnea Rudenius: 2020 : 
-            <a css={{color: '#eabe53'}} href="/cdn-cgi/l/email-protection#e38f8a8d8d8682cd91a38b8c978e828a8fcd808c8e">email</a> : 
-            <a css={{color: '#eabe53'}} href="https://www.linkedin.com/in/linnea-rudenius-b67a47152"> linkedIn </a>
+    <div
+        css={{
+            background: `url(${require("./images/no_way_through.jpg")}) no-repeat center center fixed`,
+            "-webkit-background-size": "cover",
+            "-moz-background-size": "cover",
+            "-o-background-size": "cover",
+            height: "100vh",
+            with: "100vw",
+        }}
+    >
+        <div
+            css={{
+                position: "absolute",
+                bottom: 5,
+                color: "#eabe53",
+                fontFamily: "Indie Flower, cursive",
+                padding: "0 10px",
+                backgroundColor: "#22291f",
+            }}
+        >
+            Linnea Rudenius: 2020 :
+            <a
+                css={{ color: "#eabe53" }}
+                href="/cdn-cgi/l/email-protection#e38f8a8d8d8682cd91a38b8c978e828a8fcd808c8e"
+            >
+                email
+            </a>{" "}
+            :
+            <a
+                css={{ color: "#eabe53" }}
+                href="https://www.linkedin.com/in/linnea-rudenius-b67a47152"
+            >
+                {" "}
+                linkedIn{" "}
+            </a>
         </div>
-        <div css={{
-            position: 'absolute',
-            bottom: 5,
-            right: 5,
-            color: '#eabe53',
-            fontFamily: 'Indie Flower, cursive',
-            padding: '0 10px',
-            backgroundColor: '#22291f',
-        }}>
-            <a href="/birds" css={{color: '#eabe53'}}>Birds</a>
+        <div
+            css={{
+                position: "absolute",
+                bottom: 5,
+                right: 5,
+                color: "#eabe53",
+                fontFamily: "Indie Flower, cursive",
+                padding: "0 10px",
+                backgroundColor: "#22291f",
+            }}
+        >
+            <a href="/birds" css={{ color: "#eabe53" }}>
+                Birds
+            </a>
         </div>
     </div>
-)
+);
 
-export default StartPage
+export default StartPage;
