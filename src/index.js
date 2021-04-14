2b7f170148e46a17d89dcf41c55a623664b6b0e3
Linnea Rudenius <linnea.rudenius@epidemicsound.com>
2021-04-14 18:03:06 +0200

    tmP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './indexss.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
